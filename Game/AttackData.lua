ebb1d9d7064e66e401e7f9048c02b841e8957c11
Dean <deanyockey@yahoo.com>
2025-07-17 20:24:32 -0700

    Increased hitfreeze a little

--- Game/AttackData.lua
+++ Game/AttackData.lua
@@ -14,7 +14,7 @@ function AttackData(damage, stun, knockback, flags, hitfreeze)
 end
 
 function AttackData_Power(power)
-    local attackData = AttackData(10 + power*10, 14 + power*3, 150 + power*30, 0, 5 + power*3)
+    local attackData = AttackData(10 + power*10, 14 + power*3, 150 + power*30, 0, 5 + power*5)
 
     return attackData
 end
