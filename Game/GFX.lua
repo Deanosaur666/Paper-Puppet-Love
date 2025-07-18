966eed3cdac344f18b0febf8d72c143a2b27c0a7
Dean <deanyockey@yahoo.com>
2025-07-17 20:09:27 -0700

    Center GFX drawing

--- Game/GFX.lua
+++ Game/GFX.lua
@@ -18,11 +18,10 @@ function DrawGFX(gfx, x, y)
     local spriteCount = #spriteList
     local index = math.floor(Clamp((gfx.Time/gfx.Lifetime)*spriteCount + 1, 1, spriteCount))
 
-    print(index)
-
     local sprite = spriteList[index]
+    local w, h = sprite:getDimensions( )
 
-    love.graphics.draw(sprite, gfx.X, gfx.Y)
+    love.graphics.draw(sprite, gfx.X - w/2, gfx.Y - h/2)
 
     gfx.Time = gfx.Time + 1
 end
