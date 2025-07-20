fa2150b11db0b6a8195b25e511aa143323677830
Jimbo! <yockeyjim@yahoo.com>
2025-07-19 20:54:24 -0700

    In the middle of CSV stuff

--- CSV.lua
+++ CSV.lua
@@ -2,6 +2,7 @@
 function SplitCSVLine(line)
 	local values = {}
 
+	--[[
 	for value in line:gmatch("[^,]+") do -- Note: We won't match empty values.
 		-- Convert the value string to other Lua types in a "smart" way.
 		if     tonumber(value)  then  table.insert(values, tonumber(value)) -- Number.
@@ -10,6 +11,36 @@ function SplitCSVLine(line)
 		else                          table.insert(values, value)           -- String.
 		end
 	end
+	]]
+	local startIndex = 1
+	local nextComma = nil
+	repeat
+		nextComma = string.find(line, ",", startIndex+1 or 1)
+		print("Next Comma" .. tostring(nextComma))
+
+		if(nextComma ~= nil and nextComma > startIndex) then
+			local str = string.sub(line, startIndex, nextComma-1)
+			if (tonumber(str))  then 
+				table.insert(values, tonumber(str)) -- Number.
+			elseif (str == "true")  then
+				table.insert(values, true)            -- Boolean.
+			elseif (str == "false") then
+				table.insert(values, false)           -- Boolean.
+			elseif(str == "") then
+				table.insert(values, nil)
+			elseif(str == ",") then
+				table.insert(values, nil)
+				--startIndex = startIndex - 1
+			else
+				table.insert(values, str)           -- String.
+			end
+
+			startIndex = nextComma + 1
+		else
+			nextComma = nil
+		end
+
+	until nextComma == nil
 
 	return values
 end
@@ -41,6 +72,8 @@ function CSVToTables(csv)
         for column, value in ipairs(csv[i]) do
             tableData[i-1] = {}
             tableData[i-1][tableHeader[column]] = value
+
+			print("Index: " .. tableHeader[column] .. "; Value: "  .. tostring(value))
         end
     end
 
