4c149606c0e623103b669498750e61564c4b451c
Jimbo! <yockeyjim@yahoo.com>
2025-07-19 22:30:20 -0700

    Trying to parse stances

--- CSV.lua
+++ CSV.lua
@@ -35,7 +35,7 @@ function CSVToTables(csv)
             tableData[i-1] = {}
             tableData[i-1][tableHeader[column]] = value
 
-			print("Index: " .. tableHeader[column] .. "; Value: "  .. tostring(value))
+			--print("Index: " .. tableHeader[column] .. "; Value: "  .. tostring(value))
         end
     end
 
@@ -46,18 +46,45 @@ end
 -- takes the converted CSV table and tries to turn it into a valid table of attacks
 function ParseAttackTable(table, fighter)
 	local stances = {}
-	local lastFighterRow = nil
 
 	local myAttacks = {}
 
 	
-	
+	-- we run through and catalog every stance existing in the sheet
 	for row, line in ipairs(table) do
-		if(line.FighterIndex == fighter and lastFighterRow == nil) then
-			lastFighterRow = line.FighterIndex
-		elseif(line.FighterIndex ~= nil) then
-
+		if(line.ReqStances ~= nil and line.ReqStances ~= "") then
+			local stanceFrom = stringsplit(line.ReqStances, ";", true, true)
+			print(stanceFrom)
+			for _, sf in ipairs(stanceFrom) do
+				if(not tableContains(stances, sf)) then
+					table.insert(stances, sf)
+				end
+			end
 		end
+		
 
+		if(line.Stances ~= nil and line.Stances ~= "") then
+			local stanceTo =  stringsplit(line.Stances, ";", true, true)
+			for _, st in ipairs(stanceTo) do
+				if(not tableContains(stances, st)) then
+					table.insert(stances, st)
+				end
+			end
+		end
+		
 	end
+
+	print("Stances...")
+	for index, value in ipairs(stances) do
+		print("Stance: " .. value)
+	end
+end
+
+function tableContains(table, value)
+  for i = 1,#table do
+    if (table[i] == value) then
+      return true
+    end
+  end
+  return false
 end
